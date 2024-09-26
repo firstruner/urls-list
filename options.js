f6360efacfd17c07c9d17950e028c295ef28e76b
Christophe@chamborelle.local <christophe@chamborelle.fr>
2024-09-26 17:34:07 +0200

    INFORMATIQUE-christophe (26 sept. 2024 17:34)

--- options.js
+++ options.js
@@ -1,8 +1,14 @@
+function browseResult(e){
+  var fileselector = document.getElementById('outputSaveFolder');
+  console.log(fileselector.value);
+}
+
 function saveOptions(e) {
   e.preventDefault();
   browser.storage.sync.set({
     showTabContextMenuCopyUrls: document.querySelector("#showTabContextMenuCopyUrls").checked,
     openUrlsAlreadyOpened: document.querySelector("#openUrlsAlreadyOpened").checked,
+    outputSaveFolder: document.querySelector("#outputSaveFolder").value,
   });
   browser.runtime.sendMessage({});
 }
@@ -11,8 +17,11 @@ function restoreOptions() {
   browser.storage.sync.get().then(settings => {
     let showContextMenu = ('showTabContextMenuCopyUrls' in settings) ? settings.showTabContextMenuCopyUrls : true;
     let openTabs = ('openUrlsAlreadyOpened' in settings) ? settings.openUrlsAlreadyOpened : false;
+    let outputSaveFolder = ('outputSaveFolder' in settings) ? settings.openUrlsAlreadyOpened : "";
+
     document.querySelector("#showTabContextMenuCopyUrls").checked = showContextMenu;
     document.querySelector("#openUrlsAlreadyOpened").checked = openTabs;
+    document.querySelector("#outputSaveFolder").value = outputSaveFolder;
   }, error => {
     console.log(`Error: ${error}`);
   });
